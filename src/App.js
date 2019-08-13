11ebcf7638ab96468db08516bbd4fd48cd39cc34
Noah Waldner <noah.waldner@bkb.ch>
2019-08-13 16:35:31 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const { ipcRenderer } = window.require("electron");
+
+const {
+  CATCH_ON_RENDER,
+  CATCH_ON_MAIN
+} = require("./utils/constants")
+
+
+
 
 function App() {
+  const handleMessage = (event, data) => {
+    console.log(data);
+  }
+
+  React.useState(() => {
+    ipcRenderer.on(CATCH_ON_RENDER, handleMessage)
+    return (false);
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +29,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={() => {
+          console.log("click ");
+          ipcRenderer.send(CATCH_ON_MAIN, "test string")
+        }}>SEND</button>
         <a
           className="App-link"
           href="https://reactjs.org"
